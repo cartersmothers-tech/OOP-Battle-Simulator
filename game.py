b0e17e45f6492ede44f1f2c1923de04cd20289b8
cartersmothers-tech <carter.smothers@ascte.org>
2026-09-11 10:23:21 -0500

    Update game.py
    Added new Goblin

--- game.py
+++ game.py
@@ -10,9 +10,13 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Bean")
+    goblin = Goblin("Big Bean")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
+    goblinTwo = Goblin("Little Bean")
+    
+    print(f"{goblinTwo.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
 
 
